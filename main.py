d2123267ca15bbe0676344062d32e8f2686ab131
jlgonrod <jlgonzalezrodriguez@uoc.edu>
2022-04-06 23:16:45 +0200

    Se pasa la función que cierra el navegador al método __del__(self) de la clase Crawler, con ello se puede usar el método get_amount_results() y get_list_references() del objeto sin problemas por que se cierre el navegador antes de llamarse.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ scraping = Crawler("Codeina")
 lista = scraping.get_list_references()
 
 #Ventaja tarda menos no tiene que comprobar todos los resultados con este metodo
-#numero = scraping.get_amount_results()
+numero = scraping.get_amount_results()
 
-#print(numero)
+print(numero)
 print(lista)
